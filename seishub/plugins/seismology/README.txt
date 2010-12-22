52ef223111bbc74f41661c0a97cc9ddf55007eaf
Robert Barsch <mail@robert-barsch.de>
2010-12-22 01:26:15 +0000

    - license updates - updated dependencies for seishub.plugins and seishub.core

---
+++ seishub/plugins/seismology/README.txt
@@ -0,0 +1,33 @@
+package seishub.plugins.seismology
+==================================
+
+Copyright
+---------
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+
+    Copyright (c) 2007-2011 by:
+        * Robert Barsch
+        * Moritz Beyreuther
+        * Tobias Megies 
+
+
+Overview
+--------
+    seishub.plugins.seismology - Seismology package for SeisHub.
+
+    For more information visit http://www.seishub.org.
+
+
+Dependencies
+------------
+    * setuptools
+    * seishub.core
+    * obspy.core
+    * obspy.mseed
+    * obspy.gse2
+    * obspy.seishub
+    * obspy.imaging
+    * obspy.xseed
+    * obspy.arclink
+    * obspy.db (https://svn.obspy.org/obspy.db/trunk#egg=obspy.db-dev)
--- setup.py
+++ setup.py
@@ -1,49 +1,71 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (c) 2008-2009 by:
-#     * Robert Barsch
-#
-# GNU General Public License (GPL)
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-
 """
-setup.py bdist_egg
+SeisHub installer
+
+:copyright:
+    Robert Barsch (barsch@lmu.de)
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
 """
 
 from setuptools import setup
+import os
+
+
+VERSION = open(os.path.join("seishub", "plugins", "seismology",
+                            "VERSION.txt")).read()
 
 
 setup(
     name='seishub.plugins.seismology',
-    version='0.0.1',
-    description="",
-    long_description="""""",
-    classifiers=[],
-    keywords='',
+    version=VERSION,
+    description="Seismology package for SeisHub.",
+    long_description="""
+    seishub.plugins.seismology - Seismology package for SeisHub.
+
+    For more information visit http://www.seishub.org.
+    """,
+    url='http://www.seishub.org',
     author='Robert Barsch',
-    author_email='barsch@geophysik.uni-muenchen.de',
-    url='https://svn.geophysik.uni-muenchen.de/svn/seishub',
-    license='BSD',
+    author_email='barsch@lmu.de',
+    license='GNU Lesser General Public License, Version 3 (LGPLv3)',
+    platforms='OS Independent',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ' + \
+        'GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    keywords=['SeisHub', 'seismology'],
     packages=['seishub.plugins.seismology'],
     namespace_packages=['seishub', 'seishub.plugins'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'seishub.core',
+        'obspy.core',
+        'obspy.mseed',
+        'obspy.gse2',
+        'obspy.seishub',
+        'obspy.imaging',
+        'obspy.xseed',
+        'obspy.arclink',
+        'obspy.db',
     ],
-    entry_points={'seishub.plugins': ['seishub.plugins.seismology = seishub.plugins.seismology', ]},
+    dependency_links=[
+        "https://svn.obspy.org/obspy.db/trunk#egg=obspy.db-dev"
+    ],
+    download_url="https://svn.geophysik.uni-muenchen.de/svn/seishub/trunk/seishub.plugins.seismology#egg=seishub.plugins.seismology-dev",
+    entry_points={'seishub.plugins': [
+        'seishub.plugins.seismology = seishub.plugins.seismology', ]
+    },
 )
