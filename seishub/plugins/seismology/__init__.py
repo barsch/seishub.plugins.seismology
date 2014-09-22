be14dcdc6c35bebacc221f8bc4a640b57e433046
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-09-22 15:04:55 +0200

    use static version number from VERSION.TXT

--- seishub/plugins/seismology/__init__.py
+++ seishub/plugins/seismology/__init__.py
@@ -4,3 +4,7 @@ from package import *
 from station import *
 from event import *
 from waveform import *
+
+
+path = os.path.dirname(__file__)
+__version__ = open(os.path.join(path, "VERSION.txt")).read().strip()
